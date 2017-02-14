6a8b6a8242a3d3dfd17dd26af1ccb4ec7a8d4028
soroush yousefi <soroush.ysf@gmail.com>
2017-02-14 19:46:40 +0330

    production change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+    config.reload_controllers = Rails.env.development?
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
@@ -24,7 +25,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+ # config.assets.compile = false
     #showing css images
     config.serve_static_assets = true
     config.assets.compile = true
