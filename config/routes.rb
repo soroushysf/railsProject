5d6048272d9e398a845cfd6343d9a5020bb1182c
soroush yousefi <soroush.ysf@gmail.com>
2017-02-14 19:27:43 +0330

    route chang

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :microposts
   resources :users
+    resources :intro 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root "intro#welcome"
 end
