e2b0af34b85e7d43cfe3dcdee67bfa044130b945
soroush yousefi <soroush.ysf@gmail.com>
2017-02-14 19:20:41 +0330

    intro page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'intro/welcome'
+
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    root "users#index"
+    root "intro#welcome"
 end
