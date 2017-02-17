b57d479bc036ce940abdbc8b1f728b1d8c60208b
soroush yousefi <soroush.ysf@gmail.com>
2017-02-17 15:18:08 +0330

    routes change

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
     root "intro#welcome"
-    get "intro/welcome"
+    #get "intro/welcome"
 
   resources :microposts
   resources :users
-    resources :intro 
+#    resources :intro 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
